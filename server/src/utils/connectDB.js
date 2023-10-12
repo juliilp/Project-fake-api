306843b9ae8708c07fa7bde86d1bf3199299cb06
Julian Lopez Padua <julianlopez43013@gmail.com>
2023-10-12 00:18:44 -0300

    variables de entorno

--- server/src/utils/connectDB.js
+++ server/src/utils/connectDB.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
-
+require("dotenv").config();
+const { MONGODB_CONNECT } = process.env;
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/mercadopago");
+    await mongoose.connect(MONGODB_CONNECT);
     console.log("Base de datos conectada");
   } catch (error) {
-    console.log(error);
+    console.log("Error" + error.message);
   }
 };
 
