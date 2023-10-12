5177fa60a60775926ebf75a17ab4c46325363503
Julian Lopez Padua <julianlopez43013@gmail.com>
2023-10-12 00:38:15 -0300

    server -> index

--- server/server.js
+++
@@ -1,8 +0,0 @@
-const server = require("./src/index");
-const connectDB = require("./src/utils/connectDB");
-require("dotenv").config();
-
-server.listen(process.env.PORT, () => {
-  console.log("Server levantado en " + process.env.PORT);
-  connectDB();
-});
