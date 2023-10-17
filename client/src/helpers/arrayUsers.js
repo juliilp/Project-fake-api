cbd7dfab2e5304d5f81e7a0ce0ad5a17c8b6458b
Julian Lopez Padua <julianlopez43013@gmail.com>
2023-10-17 19:11:27 -0300

    test user

---
+++ client/src/helpers/arrayUsers.js
@@ -0,0 +1,20 @@
+const arrayUsers = [
+  {
+    cantidad: 1,
+    users: [
+      {
+        _id: "65276e5b86e58dc3ddd67863",
+        email: "testadmin@gmail.com",
+        password:
+          "$2b$10$n5UWbVYQBE1UraHzXuYdN.sI0ZSHzzOf5Fc3xuQT88UoOL3yFoBUi",
+        isAdmin: true,
+        isBaneable: false,
+        __v: 0,
+      },
+    ],
+    totalPages: 1,
+    currentPage: 1,
+  },
+];
+
+export default arrayUsers;
