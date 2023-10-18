565498ba12aef1a492a3ad87ec52a8b7624a6f82
Julian Lopez Padua <julianlopez43013@gmail.com>
2023-10-18 19:46:44 -0300

    animaciones card

--- client/src/components/MenuDesktop.jsx
+++ client/src/components/MenuDesktop.jsx
@@ -1,10 +1,18 @@
 export default function MenuDesktop() {
   return (
     <header className="hidden md:flex gap-6 w-full justify-center items-center  h-28">
-      <a href="/">Inicio</a>
-      <a href="/users">Usuarios</a>
-      <a href="/productos">Productos</a>
-      <a href="/mercadopago">Mercado pago</a>
+      <a href="/" className="hover:bg-[#576374] p-3  duration-300">
+        Inicio
+      </a>
+      <a href="/users" className="hover:bg-[#576374] p-3  duration-300">
+        Usuarios
+      </a>
+      <a href="/productos" className="hover:bg-[#576374] p-3  duration-300">
+        Productos
+      </a>
+      <a href="/mercadopago" className="hover:bg-[#576374] p-3  duration-300">
+        Mercado pago
+      </a>
     </header>
   );
 }
