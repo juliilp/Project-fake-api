5da4b39e0c6cbb8cdaa95b8a83e4f7a4caf9f258
Julian Lopez Padua <julianlopez43013@gmail.com>
2023-10-19 14:42:01 -0300

    boiler plate user

--- client/src/components/MenuDesktop.jsx
+++ client/src/components/MenuDesktop.jsx
@@ -1,16 +1,28 @@
 export default function MenuDesktop() {
   return (
     <header className="hidden md:flex gap-6 w-full justify-center items-center  h-28">
-      <a href="/" className="hover:bg-[#576374] p-3  duration-300">
+      <a
+        href="/"
+        className="hover:bg-[#576374] p-3   active:text-[#3895F2] duration-300"
+      >
         Inicio
       </a>
-      <a href="/users" className="hover:bg-[#576374] p-3  duration-300">
+      <a
+        href="/users"
+        className="hover:bg-[#576374] p-3   active:text-[#3895F2] duration-300"
+      >
         Usuarios
       </a>
-      <a href="/productos" className="hover:bg-[#576374] p-3  duration-300">
+      <a
+        href="/productos"
+        className="hover:bg-[#576374] p-3   active:text-[#3895F2] duration-300"
+      >
         Productos
       </a>
-      <a href="/mercadopago" className="hover:bg-[#576374] p-3  duration-300">
+      <a
+        href="/mercadopago"
+        className="hover:bg-[#576374] p-3   active:text-[#3895F2] duration-300"
+      >
         Mercado pago
       </a>
     </header>
