69e40501b0d2d2e37a51909abd3e750682f7fa78
Julian Lopez Padua <julianlopez43013@gmail.com>
2023-10-19 17:49:56 -0300

    map user

--- client/src/helpers/arrayUsers.js
+++ client/src/helpers/arrayUsers.js
@@ -1,19 +1,27 @@
 const arrayUsers = [
   {
-    cantidad: 1,
-    users: [
+    titulo: "Todos los usuarios",
+    descripcion:
+      "Podes acceder a todos los usuarios y con su respectivas queryes de limit y page",
+    request: "[GET] https://fake-api-project-backend.onrender.com/user/all",
+    response: [
       {
-        _id: "65276e5b86e58dc3ddd67863",
-        email: "testadmin@gmail.com",
-        password:
-          "$2b$10$n5UWbVYQBE1UraHzXuYdN.sI0ZSHzzOf5Fc3xuQT88UoOL3yFoBUi",
-        isAdmin: true,
-        isBaneable: false,
-        __v: 0,
+        cantidad: 1,
+        users: [
+          {
+            _id: "65276e5b86e58dc3ddd67863",
+            email: "testadmin@gmail.com",
+            password:
+              "$2b$10$n5UWbVYQBE1UraHzXuYdN.sI0ZSHzzOf5Fc3xuQT88UoOL3yFoBUi",
+            isAdmin: true,
+            isBaneable: false,
+            __v: 0,
+          },
+        ],
+        totalPages: 1,
+        currentPage: 1,
       },
     ],
-    totalPages: 1,
-    currentPage: 1,
   },
 ];
 
