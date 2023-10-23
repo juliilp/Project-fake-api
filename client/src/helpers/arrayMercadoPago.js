f8894c35a116acaa0df954a3d19f4f9e054b2ba6
Julian Lopez Padua <julianlopez43013@gmail.com>
2023-10-23 13:05:28 -0300

    respuestas mercadopago

--- client/src/helpers/arrayMercadoPago.js
+++ client/src/helpers/arrayMercadoPago.js
@@ -5,35 +5,29 @@ const arrayMercadoPago = [
       "Utiliza esta ruta para crear una orden de pago en Mercado Pago.",
     request:
       "[POST] https://fake-api-project-backend.onrender.com/create-order",
-    response: "Respuesta no especificada en el README.",
+    response:
+      "https://www.mercadopago.com.ar/checkout/v1/redirect?pref_id=1494563427-8d8c7142-1aa8-4c8c-8659-2bfe33cbed67",
   },
   {
     titulo: "Éxito de pago",
     descripcion:
       "Esta ruta se utiliza para manejar el éxito de una transacción de Mercado Pago.",
     request: "[GET] https://fake-api-project-backend.onrender.com/success",
-    response: "Respuesta no especificada en el README.",
+    response: "A editar según gusto",
   },
   {
     titulo: "Error de pago",
     descripcion:
       "Esta ruta se utiliza para manejar errores en transacciones de Mercado Pago.",
     request: "[GET] https://fake-api-project-backend.onrender.com/failure",
-    response: "Respuesta no especificada en el README.",
+    response: "A editar según gusto",
   },
   {
     titulo: "Pago pendiente",
     descripcion:
       "Esta ruta se utiliza para manejar pagos pendientes en transacciones de Mercado Pago.",
     request: "[GET] https://fake-api-project-backend.onrender.com/pending",
-    response: "Respuesta no especificada en el README.",
-  },
-  {
-    titulo: "Webhook de Mercado Pago",
-    descripcion:
-      "Esta ruta recibe notificaciones de webhook de Mercado Pago para actualizar el estado de las transacciones.",
-    request: "[POST] https://fake-api-project-backend.onrender.com/webhook",
-    response: "Respuesta no especificada en el README.",
+    response: "A editar según gusto",
   },
 ];
 
